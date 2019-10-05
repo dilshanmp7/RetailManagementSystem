2d3222d12b94bc990d01afbca19a1dca9e4ea76f
Dilshan Makavitage <dilshanmp7@gmail.com>
2019-10-05 14:34:30 -0700

    Add Retial manager desktop app

---
+++ RetailManagerApp/BootStrapper.cs
@@ -0,0 +1,26 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using RetailManagerApp.ViewModels;
+
+namespace RetailManagerApp
+{
+    public class BootStrapper :BootstrapperBase
+    {
+        public BootStrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+
+    }
+}
