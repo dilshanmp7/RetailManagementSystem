5e2da8356e7a4ab61b660cce8321e86b7b2368ba
Dilshan Makavitage <dilshanmp7@gmail.com>
2019-10-05 21:29:23 -0700

    implement Dependency injection

--- RetailManagerApp/BootStrapper.cs
+++ RetailManagerApp/BootStrapper.cs
@@ -11,9 +11,26 @@ namespace RetailManagerApp
 {
     public class BootStrapper :BootstrapperBase
     {
+        private SimpleContainer _container = new SimpleContainer();
+
         public BootStrapper()
         {
             Initialize();
+        }
+
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes()
+                .Where(type=>type.IsClass)
+                .Where(type=>type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(vmType=>_container.RegisterPerRequest(
+                    vmType,vmType.ToString(),vmType));
         }
 
         protected override void OnStartup(object sender, StartupEventArgs e)
@@ -21,6 +38,19 @@ namespace RetailManagerApp
             DisplayRootViewFor<ShellViewModel>();
         }
 
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);  
+        }
 
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance); 
+        }
     }
 }
